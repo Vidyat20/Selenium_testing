3ce3535d381061d6b7b094d1fbd86c45448b9a62
vidyat20 <vidyabelaku51@gmail.com>
2025-10-24 20:56:33 +0530

    Updated Jenkinsfile and fixed verifyLogin test

--- src/test/java/learntesting/first.java
+++ src/test/java/learntesting/first.java
@@ -6,8 +6,9 @@ import org.testng.annotations.Test;
 
 public class first extends BaseTest {
     @Test
-    public void verifyLogin() {
+    public void verifyLogin() throws Throwable {
         driver.get("https://ui.portalqa.exostartest.com/iamui/mfa/userReference");  // driver is now initialized
+        Thread.sleep(10000);
         driver.findElement(By.xpath("//input[@data-placeholder='Email Address or User ID']")).sendKeys("bishtt_4356");
         driver.findElement(By.xpath("//span[contains(text(),'NEXT')]")).click();
         driver.findElement(By.xpath("//input[@data-placeholder='Password']")).sendKeys("Jai_mata_di@1234");
