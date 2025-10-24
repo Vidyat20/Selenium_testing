9017ede978e9ec53e37dc85c4865812c2ee3fca6
vidyat20 <vidyabelaku51@gmail.com>
2025-10-24 18:40:04 +0530

    Initial commit: Selenium + TestNG project

---
+++ src/test/java/learntesting/first.java
@@ -0,0 +1,19 @@
+package learntesting;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import learntestings.BaseTest;
+
+public class first extends BaseTest {
+    @Test
+    public void verifyLogin() {
+        driver.get("https://ui.portalqa.exostartest.com/iamui/mfa/userReference");  // driver is now initialized
+        driver.findElement(By.xpath("//input[@data-placeholder='Email Address or User ID']")).sendKeys("bishtt_4356");
+        driver.findElement(By.xpath("//span[contains(text(),'NEXT')]")).click();
+        driver.findElement(By.xpath("//input[@data-placeholder='Password']")).sendKeys("Jai_mata_di@1234");
+        driver.findElement(By.xpath("//span[contains(text(),'NEXT')]")).click();
+
+    }
+}
