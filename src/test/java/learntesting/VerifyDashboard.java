2ac7229dc8a93a71059758c888ed8a329d4565e6
vidyat20 <vidyabelaku51@gmail.com>
2026-04-14 15:20:25 +0530

    final code push on 14th april

--- src/test/java/learntesting/VerifyDashboard.java
+++ src/test/java/learntesting/VerifyDashboard.java
@@ -21,39 +21,39 @@ public void verifymenus () throws Throwable {
 	System.out.println("dashboard is verified");
 	
 }
-	@Test
-public void elevate_creadentials() {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@id='nnl_iframe']")));
-		driver.switchTo().frame("nnl_iframe");
-		WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement elevate_creds = wait1.until(
-		    ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Elevate Credential Strength')]"))
-		);
-		elevate_creds.click();
+//	@Test
+//public void elevate_creadentials() {
+//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+//		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@id='nnl_iframe']")));
+//		driver.switchTo().frame("nnl_iframe");
+//		WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
+//		WebElement elevate_creds = wait1.until(
+//		    ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Elevate Credential Strength')]"))
+//		);
+//		elevate_creds.click();
+//	
+//	//verify Two-step verification text
+//	WebElement twostep = driver.findElement(By.xpath("//div/h2[contains(text(),'Two-Step Verification')]"));
+//			twostep.getText();
+//			System.out.println(twostep);
+//    //verify send text radio button is selected by default
+//			WebElement verify_sendtext = driver.findElement(By.xpath("//mat-radio-button[@id='mat-radio-0']"));
+//			verify_sendtext.isSelected();
+//			if(verify_sendtext.isSelected()) {
+//				System.out.println("Send text radio button is selected");
+//			}else {
+//				System.out.println("Send text radio button is not selected");
+//				WebElement Select_sendtext = driver.findElement(By.xpath("//input[@id='mat-radio-0-input']"));
+//				Select_sendtext.click();
+//			}
+//    WebElement Click_on_send = driver.findElement(By.xpath("//span[normalize-space()='SEND']"));
+//    Click_on_send.click();
+//    WebElement Enter_PhoneOTP = driver.findElement(By.xpath("//input[@autocomplete='PhoneOTP']"));
+//    Enter_PhoneOTP.sendKeys("11111111");
+//    System.out.println("elevate credentials page");
+//    
 	
-	//verify Two-step verification text
-	WebElement twostep = driver.findElement(By.xpath("//div/h2[contains(text(),'Two-Step Verification')]"));
-			twostep.getText();
-			System.out.println(twostep);
-    //verify send text radio button is selected by default
-			WebElement verify_sendtext = driver.findElement(By.xpath("//mat-radio-button[@id='mat-radio-0']"));
-			verify_sendtext.isSelected();
-			if(verify_sendtext.isSelected()) {
-				System.out.println("Send text radio button is selected");
-			}else {
-				System.out.println("Send text radio button is not selected");
-				WebElement Select_sendtext = driver.findElement(By.xpath("//input[@id='mat-radio-0-input']"));
-				Select_sendtext.click();
-			}
-    WebElement Click_on_send = driver.findElement(By.xpath("//span[normalize-space()='SEND']"));
-    Click_on_send.click();
-    WebElement Enter_PhoneOTP = driver.findElement(By.xpath("//input[@autocomplete='PhoneOTP']"));
-    Enter_PhoneOTP.sendKeys("11111111");
-    System.out.println("elevate credentials page");
-    
-	
-}
+//}
 
 
 }
